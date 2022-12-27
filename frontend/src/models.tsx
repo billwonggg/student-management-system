4eee36fbb583a56d1f67b31feb897b62acc81191
Bill Wong <billwong7788@gmail.com>
2022-12-28 08:46:51 +1100

    add api response model

---
+++ frontend/src/models.tsx
@@ -0,0 +1,12 @@
+interface studentResponse {
+  id: number;
+  firstname: string;
+  surname: string;
+  email: string;
+  dob: Date;
+  gender: string;
+  mobile: string;
+  age: number;
+}
+
+export default studentResponse;
