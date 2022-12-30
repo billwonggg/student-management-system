1b9b6d80e2868121e31b22732c730ab09a81e741
Bill Wong <billwong7788@gmail.com>
2022-12-30 15:40:59 +1100

    move helpers to own file

---
+++ frontend/src/Helpers/ApiHelpers.tsx
@@ -0,0 +1,21 @@
+import { GridCellEditCommitParams } from "@mui/x-data-grid";
+import axios from "axios";
+import React from "react";
+import studentResponse from "../Models/student";
+
+export const getStudents = async (
+  setStudents: React.Dispatch<React.SetStateAction<studentResponse[]>>
+) => {
+  const endpoint = "/api/v1/student";
+  try {
+    const response = await axios.get<studentResponse[]>(endpoint);
+    setStudents(response.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const editStudents = async (params: GridCellEditCommitParams) => {
+  const endpoint = `/api/v1/student/${params.id}?${params.field}=${params.value}`;
+  axios.put(endpoint);
+};
