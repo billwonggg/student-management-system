79b740dcce3754859f3602364e2cbb5b25418acc
Bill Wong <billwong7788@gmail.com>
2022-12-30 15:42:03 +1100

    use new components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,24 @@
+import { Button, Typography } from "@mui/material";
 import axios from "axios";
 
 import { useEffect, useState } from "react";
 import StudentTable from "./Components/StudentTable";
+import { getStudents } from "./Helpers/ApiHelpers";
 // import RegistrationForm from "./Components/RegistrationForm";
 import studentResponse from "./Models/student";
 
 const App = () => {
   const [students, setStudents] = useState<studentResponse[]>([]);
 
-  const apiCall = async () => {
-    const endpoint = "/api/v1/student";
-    try {
-      const response = await axios.get<studentResponse[]>(endpoint);
-      setStudents(response.data);
-    } catch (err) {
-      console.error(err);
-      return [];
-    }
-  };
-
   useEffect(() => {
-    apiCall();
+    getStudents(setStudents);
   }, []);
 
   return (
     <div>
       {/* <RegistrationForm /> */}
+      <Typography>Students</Typography>
+      <Button onClick={() => getStudents(setStudents)}>Refresh</Button>
       <StudentTable students={students} />
     </div>
   );
