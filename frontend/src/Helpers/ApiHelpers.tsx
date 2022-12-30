05bb4d5a0f53cbcccb2d77c0e0ef3198c8b68779
Bill Wong <billwong7788@gmail.com>
2022-12-30 21:49:40 +1100

    add notification to api helpers

--- frontend/src/Helpers/ApiHelpers.tsx
+++ frontend/src/Helpers/ApiHelpers.tsx
@@ -2,25 +2,39 @@ import { GridCellEditCommitParams } from "@mui/x-data-grid";
 import axios from "axios";
 import React from "react";
 import studentResponse from "../Models/student";
+import { sendNotif } from "./Notification";
 
 export const getStudents = async (
-  setStudents: React.Dispatch<React.SetStateAction<studentResponse[]>>
+  setStudents: React.Dispatch<React.SetStateAction<studentResponse[]>>,
+  showNotif: boolean = false
 ) => {
   const endpoint = "/api/v1/student";
   try {
     const response = await axios.get<studentResponse[]>(endpoint);
     setStudents(response.data);
+    if (showNotif) sendNotif("Successfully refreshed data", "success");
   } catch (err) {
     console.error(err);
+    if (showNotif) sendNotif("Error refreshing data", "error");
   }
 };
 
 export const editStudents = async (params: GridCellEditCommitParams) => {
   const endpoint = `/api/v1/student/${params.id}?${params.field}=${params.value}`;
-  await axios.put(endpoint);
+  try {
+    await axios.put(endpoint);
+    sendNotif(`Successfully updated ${params.field}`, "success");
+  } catch (err) {
+    sendNotif(`Error updating ${params.field}`, "error");
+  }
 };
 
 export const deleteStudents = async (students: string) => {
   const endpoint = `/api/v1/student/${students}`;
-  await axios.delete(endpoint);
+  try {
+    await axios.delete(endpoint);
+    sendNotif("Successfully deleted students", "success");
+  } catch (err) {
+    sendNotif("Error deleting students", "error");
+  }
 };
