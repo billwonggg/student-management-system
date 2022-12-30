c57f32bd75bf3ba91332d7d2d0e60ae6a55d20a0
Bill Wong <billwong7788@gmail.com>
2022-12-30 19:44:02 +1100

    handle selection and deletion

--- frontend/src/Components/StudentTable.tsx
+++ frontend/src/Components/StudentTable.tsx
@@ -1,6 +1,13 @@
-import { Box } from "@mui/material";
-import { DataGrid, GridCellEditCommitParams, GridColDef } from "@mui/x-data-grid";
-import { editStudents } from "../Helpers/ApiHelpers";
+import { DeleteOutline } from "@mui/icons-material";
+import { Alert, Box, Button, IconButton, Tooltip, Typography } from "@mui/material";
+import {
+  DataGrid,
+  GridCellEditCommitParams,
+  GridColDef,
+  GridSelectionModel,
+} from "@mui/x-data-grid";
+import { useState } from "react";
+import { deleteStudents, editStudents, getStudents } from "../Helpers/ApiHelpers";
 import studentResponse from "../Models/student";
 
 const columns: GridColDef[] = [
@@ -29,6 +36,7 @@ const columns: GridColDef[] = [
 
 interface studentTableProps {
   students: studentResponse[];
+  setStudents: React.Dispatch<React.SetStateAction<studentResponse[]>>;
 }
 
 const createRow = (row: studentResponse) => {
@@ -43,19 +51,44 @@ const createRow = (row: studentResponse) => {
   };
 };
 
-const StudentTable = ({ students }: studentTableProps) => {
+const StudentTable = ({ students, setStudents }: studentTableProps) => {
   const rows = students.map((student) => createRow(student));
+  const [selected, setSelected] = useState<number[]>([]);
 
   return (
-    <Box sx={{ height: "500px", width: "80%" }}>
+    <Box>
+      <Button variant="outlined" sx={{ mb: 3 }} onClick={() => getStudents(setStudents)}>
+        Refresh Data
+      </Button>
+
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        autoHeight
         onCellEditCommit={(params: GridCellEditCommitParams) => editStudents(params)}
+        onSelectionModelChange={(selectionModel: GridSelectionModel) => {
+          setSelected(selectionModel as number[]);
+        }}
       />
+      {selected.length > 0 && (
+        <Tooltip
+          title={<Typography variant="subtitle2">Delete Selected Items</Typography>}
+          sx={{ float: "right", mt: 1 }}
+        >
+          <IconButton
+            onClick={async () => {
+              await deleteStudents(selected.join(","));
+              setSelected([]);
+              await getStudents(setStudents);
+            }}
+          >
+            <DeleteOutline color="error" />
+          </IconButton>
+        </Tooltip>
+      )}
     </Box>
   );
 };
