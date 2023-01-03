ef223ab533725a8539273ae8732461856b80ec4c
Bill Wong <billwong7788@gmail.com>
2023-01-03 18:35:12 +1100

    use registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Container, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import StudentTable from "./Components/StudentTable";
-import { getStudents } from "./Helpers/ApiHelpers";
-// import RegistrationForm from "./Components/RegistrationForm";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import RegistrationForm from "./Components/RegistrationForm";
+import StudentTable from "./Components/StudentTable";
+import { getStudents } from "./Helpers/ApiHelpers";
 import studentResponse from "./Models/student";
 
 const App = () => {
@@ -17,12 +17,12 @@ const App = () => {
   return (
     <Container maxWidth="xl" sx={{ p: 2 }}>
       <ToastContainer />
-      {/* <RegistrationForm /> */}
-      <Typography variant="h2" mb={2}>
+      <Typography variant="h2" mb={2} sx={{ fontSize: "calc(1.8vw + 2vh + 1.5vmin)" }}>
         Student Management System
       </Typography>
 
       <StudentTable students={students} setStudents={setStudents} />
+      <RegistrationForm setStudents={setStudents} />
     </Container>
   );
 };
